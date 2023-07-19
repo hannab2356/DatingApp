4686cd9f029aa07b81e6e1a9603846350f821617
Hania <hboguta23567@gmail.com>
2023-07-19 16:08:05 +0200

    End of section 13

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { TimeagoModule } from 'ngx-timeago';
 import { ToastrModule } from 'ngx-toastr';
 
 
@@ -24,6 +27,9 @@ import { ToastrModule } from 'ngx-toastr';
         }),
 		FileUploadModule,
 		BsDatepickerModule.forRoot(),
+		PaginationModule.forRoot(),
+		ButtonsModule.forRoot(),
+		TimeagoModule.forRoot()
     ],
     exports: [
         BsDropdownModule,
@@ -32,7 +38,10 @@ import { ToastrModule } from 'ngx-toastr';
         NgxGalleryModule,
         NgxSpinnerModule,
 		FileUploadModule,
-		BsDatepickerModule
+		BsDatepickerModule,
+		PaginationModule,
+		ButtonsModule,
+		TimeagoModule
     ]
 })
 export class SharedModule { }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,19 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Member } from '../../_models/member';
+import { Pagination } from '../../_models/pagination';
+import { User } from '../../_models/user';
+import { UserParams } from '../../_models/userParams';
+import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 
 @Component({
-    selector: 'app-member-list',
-    templateUrl: './member-list.component.html',
-    styleUrls: ['./member-list.component.css']
+	selector: 'app-member-list',
+	templateUrl: './member-list.component.html',
+	styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-    members$: Observable<Member[]> | undefined;
+	members: Member[] = [];
+	pagination: Pagination | undefined;
+	userParams: UserParams | undefined;
+	genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
-    constructor(private membersService: MembersService) { }
+	constructor(private membersService: MembersService) {
+		this.userParams = this.membersService.getUserParams();
+	}
 
-    ngOnInit(): void {
-        this.members$ = this.membersService.getMembers();
-    }
+	ngOnInit(): void {
+		this.loadMembers();
+	}
+
+	loadMembers() {
+		if (this.userParams) {
+			this.membersService.setUserParams(this.userParams);
+			this.membersService.getMembers(this.userParams).subscribe({
+				next: response => {
+					if (response.result && response.pagination) {
+						this.members = response.result;
+						this.pagination = response.pagination;
+					}
+				}
+			})
+		}
+	}
+
+	resetFilters() {
+		this.userParams = this.membersService.resetUserParams();
+		this.loadMembers();
+	}
+
+	pageChanged(event: any) {
+		if (this.userParams && this.userParams?.pageNumber !== event.page) {
+			this.userParams.pageNumber = event.page;
+			this.membersService.setUserParams(this.userParams);
+			this.loadMembers();
+		}
+	}
 }
